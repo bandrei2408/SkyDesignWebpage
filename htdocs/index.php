351081660f8eb992d5b90fad809780e1e3889152
Bitca Andrei <bandrei2408@gmail.com>
2012-11-26 23:13:41 +0200

    Design update

---
+++ htdocs/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="styles/default.css">
+</head>
+
+<body>
+	<div id="login">
+		<div class="center">
+			<img src="images/logo.png"/>
+			<form id="loginui" method="POST" action="takelogin.php">
+				<label for="username">Username</label><input type="text" name="username" class="placeholder" placeholder="Username">
+				<label for="password">Password</label><input type="password" name="password" class="placeholder" placeholder="Password">
+				<input type="submit" value="Log in">
+			</form>
+		</div>
+	</div>
+	<div id="header">
+		
+	</div>
+	
+</body>
+
+</html>
